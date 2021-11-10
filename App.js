ae52fb3506acff66fca97937f78ecd7bb87df62c
DanVicen <danititifran89@gmail.com>
2021-11-10 15:45:49 -0300

    Se agrega al Main

--- App.js
+++ App.js
@@ -1,26 +1,55 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal } from 'react-native'
 import {AntDesing} from '@expo/vector-icons'
-import colors from './Colors';
+import colors from './Colors'
+import tempData from './tempData'
+import TodoList from './components/TodoList'
+import AddListModal from './components/AddListModal'
 
 export default class App extends React.Component {
+  state = {
+      addTodoVisible: false
+  };
+  
+  toggleAddTodoModal() {
+      this.setState({ addTodoVisible: !this.state.addTodoVisible });
+  }
+  
   render() {
     return (
         <View style={styles.container}>
+              <Modal 
+                  animationType="slide" 
+                  visible={this.state.addTodoVisible} 
+                  onRequestClose={() => this.toggleAddTodoModal()}
+              >
+                  <AddListModal closeModal={() => this.toggleAddTodoModal()} />
+             </Modal>
              <View style={{flexDirection: "row"}}>
                   <View style={styles.divider} />
-                  <Text style={styles.title}>
-                      Todo <Text style={{fontWeight: "300", color: colors.blue}}>Lists</Text>
-                  </Text>
+                    <Text style={styles.title}>
+                        Todo <Text style={{fontWeight: "300", color: colors.blue}}>Lists</Text>
+                    </Text>
                   <View style={styles.divider} />
-        </View> 
+              </View> 
         
-        <View style={{ marginVertical: 48 }}>
-            <TouchableOpacity style={styles.addList}>
-                <AntDesing name="plus" size={16} color={colors.blue} />
-            </TouchableOpacity>
+              <View style={{ marginVertical: 48 }}>
+                  <TouchableOpacity style={styles.addList onPress={() => this.toggleAddTodoModal ()}>
+                      <AntDesing name="plus" size={16} color={colors.blue} />
+                  </TouchableOpacity>
             
-            <Text style={styles.add}>Add List </Text>
+                  <Text style={styles.add}>Add List </Text>
+              </View>
+
+              <View style={{height: 275, paddingLeft: 32}}>
+                  <FlatList 
+                      data={tempData} 
+                      keyExtractor={item => item.name} 
+                      horizontal={true} 
+                      showsVerticalScrollIndicator={false}/>
+                      renderItem={({ item }) => <TodoList list={item} />}                    
+                  />
+              </View>
         </View>
     );
   }
